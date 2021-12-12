9eb434c1974271838302c1812811c8e2ccece73a
ravindar86 <ravindar.mca@gmail.com>
2021-12-12 11:32:12 +0530

    Commit for adding match page and populating match info for specific year

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import { TeamPage } from './pages/TeamPage';
+import {TeamPage} from './pages/TeamPage';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import React from 'react';
+import { MatchPage } from './pages/MatchPage';
 
 function App() {
   return (
    <div className="App">
      <Router>
        <Routes>
+        <Route path="/teams/:teamName/matches/:year" element={<MatchPage />} /> 
         <Route path="/teams/:teamName" element={<TeamPage />} />            
        </Routes>        
      </Router>
---
+++ src/pages/MatchPage.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { MatchDetailCard } from '../components/MatchDetailCard';
+
+export const MatchPage = () => {
+
+  const [matches, setMatches] = useState([]);  
+  const {teamName, year}= useParams();
+ 
+  useEffect(
+    () => {
+      const fetchMatches = async () => {
+          console.log(teamName + " " + year);
+        const response = await fetch(`http://localhost:8081/teams/${teamName}/matches?year=${year}`); // Rest API call
+        const data = await response.json();
+        setMatches(data);     // set response to the matches state
+      };
+      fetchMatches();
+    }, [teamName, year]);
+
+
+  return (
+    <div className="MatchPage">
+
+        <h1>Match Page</h1>
+        {
+            matches.map(match => <MatchDetailCard teamName={teamName} match={match} key={match.id} />)
+        }
+        
+     
+    </div>
+  );
+}
